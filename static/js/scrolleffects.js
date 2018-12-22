e8da9f90c2c81894001511cd99571afd2689f475
David Cava <david.cava.pina@gmail.com>
2018-12-22 18:22:37 +0100

    bb

--- static/js/scrolleffects.js
+++ static/js/scrolleffects.js
@@ -6,21 +6,21 @@ $(document).ready(function() {
         triggerElement: '#s1'
     })
     .setClassToggle('#t1', 'fade-in')
-    .addIndicators() // for debugging
+    // .addIndicators() // for debugging
     .addTo(controller);
 
     var scene2 = new ScrollMagic.Scene({
         triggerElement: '#s2'
     })
     .setClassToggle('#t2', 'fade-in')
-    .addIndicators() // for debugging
+    // .addIndicators() // for debugging
     .addTo(controller);
 
     var scene3 = new ScrollMagic.Scene({
         triggerElement: '#s3'
     })
     .setClassToggle('#t3', 'fade-in')
-    .addIndicators() // for debugging
+    // .addIndicators() // for debugging
     .addTo(controller);
     
 });
